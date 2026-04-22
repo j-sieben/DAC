e48974271e5c36c35c6dfeed9dd3cc97aa6faf91
Jürgen Sieben <j.sieben@condes.de>
2026-04-22 15:25:02 +0200

    Squashed 'install_scripts/' changes from 21d8352..8e8db98
    8e8db98 folders made optional
    
    git-subtree-dir: install_scripts
    git-subtree-split: 8e8db9820598c121e63000ca2a1f49290186354c

--- install/uninstall_component.sql
+++ install/uninstall_component.sql
@@ -4,10 +4,14 @@
 @&spool_dir.h1 'Begin uninstall_component'
 
 @&spool_dir.h2 'Remove existing installation (clean_up_install)'
-@&pre_dir.clean_up_install
+define optional_script = &pre_dir.clean_up_install
+define optional_missing_msg = 'keine bestehende Installation zu entfernen'
+@&help_dir.run_optional
 
 @&spool_dir.h2 'Remove application (&app_dir.uninstall_app)'
-@&app_dir.uninstall_app
+define optional_script = &app_dir.uninstall_app
+define optional_missing_msg = 'keine Anwendung zu entfernen'
+@&help_dir.run_optional
 
 @&spool_dir.h2 'Recompile schema (&help_dir.compile_schema)'
 @&help_dir.compile_schema
---
+++ scripts/helper/optional_missing.sql
@@ -0,0 +1,1 @@
+@&spool_dir.step &optional_missing_msg.
