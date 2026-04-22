8b3fe70970291b1003f7a243df2e41f34b13ea81
Jürgen Sieben <j.sieben@condes.de>
2026-04-22 15:24:55 +0200

    mat view access view installation corrected

--- DAC/Mat_views/install_mat_views.sql
+++ DAC/Mat_views/install_mat_views.sql
@@ -5,11 +5,3 @@ define mvname=dac_effective_accesses
 
 define mvname=dac_effective_access_reasons
 @&help_dir.create_mat_view
-
-prompt Creating DAC effective access views
-
-define dac_scripts_dir=&scripts_dir.
-define scripts_dir=&view_dir.
-define scriptname=install_effective_views.sql
-@&help_dir.run_script
-define scripts_dir=&dac_scripts_dir.
--- DAC/Views/install_views.sql
+++ DAC/Views/install_views.sql
@@ -23,3 +23,12 @@ define viewname=dac_access_decision_reasons_v
 
 define viewname=dac_access_decisions_v
 @&help_dir.create_view
+
+define viewname=dac_effective_accesses_v
+@&help_dir.create_view
+
+define viewname=dac_effective_access_reasons_v
+@&help_dir.create_view
+
+define viewname=dac_effective_access_status_v
+@&help_dir.create_view
